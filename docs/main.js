ffbd469cfca56bf7f3f05eb2d7c88c141da7da1f
take56789 <work.taketake@gmail.com>
2025-08-11 17:32:42 +0900

    upload

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,24 @@
-const heading = document.querySelector('#header h1');
+// 下から浮き上がらせる
+const heading = document.querySelector('.header h1');
 
 const keyframes = {
-    opacity: [0,1],
-    rotate: ['x 360deg', 0 ],
-};
-
-const options = {
-    duration: 1000,
-    easing: 'ease',
+  opacity: [0,1],
+  translate: ['0 30px', 0],
 };
 
 heading.animate(keyframes, 2000);
+
+// ふわっと表示
+const items = document.querySelectorAll ('.product-photo');
+
+for (let i =0; i < items.length; i++) {
+  const keyframes = {
+    opacity: [0,1]
+  };
+  const options = {
+    duration: 600,
+    delay: i * 300,
+    fill: 'forwards',
+  };
+  items[i].animate(keyframes,options);
+}
