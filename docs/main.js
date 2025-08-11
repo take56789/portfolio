f89bb778fcc1912a2c0139cec0a59658ad6a6cba
take56789 <work.taketake@gmail.com>
2025-08-11 16:40:17 +0900

    アップロード

--- docs/main.js
+++ docs/main.js
@@ -11,16 +11,3 @@ const options = {
 };
 
 heading.animate(keyframes, 2000);
-
-const menu = document.querySelector('#header-menu')
-const btn = document.querySelector('#hamburger')
-
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open')
-  menu.classList.toggle('open')
-  if(menu.classList.contains("open")){
-    menu.style.height = menu.scrollHeight + 'px'
-  }else{
-   menu.style.height = "0"
-  }
-})
